89bd21f3c44266081e326b89a67aa3804141a33e
Vladimir Poliakov <VladiPol@users.noreply.github.com>
2019-11-03 16:02:57 +0100

    Update V1.
    Add interaction with user

--- Demo_Output_Message.js
+++ Demo_Output_Message.js
@@ -2,10 +2,20 @@
 javax.swing.JOptionPane.showMessageDialog(null, "Hello World!!!");
 
 // für die längeren Ausgaben eine Notiz anlegen
-var vText = "";
-var vNote = model.createNote();
-vNote.setName("Ausgabe");
-vNote.setComment("1. Zeile\n2. Zeile\n");
+var v_Text = "";
+var v_Note = model.createNote();
+v_Note.setName("Ausgabe");
+v_Note.setComment("1. Zeile\n2. Zeile\n");
 
 // Der Text der Notiz im Dialog ausgeben
-vText = vNote.getComment();
+v_Text = v_Note.getComment();
+javax.swing.JOptionPane.showMessageDialog(null, v_Text);
+
+// Interaktive Aktion mit dem Benutzer
+var v_Answer = javax.swing.JOptionPane.showConfirmDialog (null , "Weiter?", "Question", javax.swing.JOptionPane.YES_NO_OPTION);
+if ( v_Answer == 0 ){
+  javax.swing.JOptionPane.showMessageDialog(null, "Answer is Yes");
+}
+else{
+  javax.swing.JOptionPane.showMessageDialog(null, "Answer is No");
+}
