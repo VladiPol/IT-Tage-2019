09f17d9b429793da0bbf1fbe1d328872aab74942
Vladimir Poliakov <VladiPol@users.noreply.github.com>
2019-12-12 23:10:11 +0100

    Add files via upload
    Some comments added

--- Put Columns from Template Table.js
+++ Put Columns from Template Table.js
@@ -5,28 +5,33 @@
 //    column even if the name of column is changed
 var t_name = "TEMPLATE_TABLE";
 var p_name = "ctemplateID";
+var t_changed = 0;
 template = model.getTableSet().getByName(t_name);
-if(template!=null){
-    tcolumns = template.getElements();
+if (template != null){
+    template_columns = template.getElements();
     tables = model.getTableSet().toArray();
-    for (var t = 0; t<tables.length;t++){
+    for (var t = 0; t < tables.length;t++){
      table = tables[t];
-    // compare name ignoring the case
+    // compare TEMPLATE_TABLE name ignoring the case
      if(!table.getName().equalsIgnoreCase(t_name)){
-         for (var i = 0; i < tcolumns.length; i++) {
-            column = tcolumns[i];
+         for (var i = 0; i < template_columns.length; i++) {
+            column = template_columns[i];
             col = table.getElementByName(column.getName());
-            if(col==null){
-             col = table.getColumnByProperty(p_name,column.getObjectID());
+            if(col == null){
+             col = table.getColumnByProperty(p_name, column.getObjectID());
             }
-            if(col==null){
+            if(col == null){
              col = table.createColumn();
             }
             column.copy(col);
             //set property after copy otherwise it'll be cleared by copy
             col.setProperty(p_name,column.getObjectID());
-            table.setDirty(true);
+            table.setDirty(true);            
          }
-     }
+      t_changed++;
+      }
     }
 }
+
+// Output in dialog
+javax.swing.JOptionPane.showMessageDialog(null, "Anzahl Tabellen im Modell: " +  (model.getTableSet().toArray().length - 1) + "\n" + "Anzahl transformierten Tabellen: " + t_changed);
